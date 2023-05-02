fbda1365a80f841d10215d456b3186d3b9f00fd7
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-05-02 22:08:22 +0600

    json-file-add

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
-const app = express()
+const app = express();
+
 const port = 3000;
+
+const chefs= require('./data/chefs.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
 })
 
 app.listen(port, () => {
