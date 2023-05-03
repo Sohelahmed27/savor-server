227fefb6a2927219d0b17cb295f824d7c662e35c
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-05-04 02:21:25 +0600

    foodItems-added

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express')
 const app = express();
-
+const cors = require('cors');
 const port = 3000;
 
+
+app.use(cors());
 const chefs= require('./data/chefs.json')
+const details = require('./data/details.json')
+const items = require('./data/foodItems.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/items', (req, res) => {
+  res.send(items)
+})
+
 
 app.get('/chefs', (req, res) => {
   res.send(chefs)
+})
+
+app.get('/details/:id', (req, res) => {
+  const id = req.params.id;
+  // res.send(details)
+  const selectedChef = details.find(c=>c.id ==id)
+  res.send(selectedChef)
 })
 
 app.listen(port, () => {
